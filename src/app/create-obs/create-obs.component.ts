01ce0157151bbf1367b1d637c9779c44d724ea96
tiwariji <tiwariyogesht@gmail.com>
2020-02-17 20:16:39 +0530

    add method to fetch response

--- src/app/create-obs/create-obs.component.ts
+++ src/app/create-obs/create-obs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from 'src/app/api.service';
 
 @Component({
   selector: 'app-create-obs',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateObsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api: ApiService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    
+    const apiResponse = fetch(`${this.api.baseURL}data`)
+    
   }
 
 }
