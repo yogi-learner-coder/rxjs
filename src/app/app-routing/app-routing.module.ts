dd61acfa42e5e1b4839c52b118e1f370087f6b69
tiwariji <tiwariyogesht@gmail.com>
2020-02-17 20:10:02 +0530

    add route for observer component

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import { ExampleComponent } from '../example/example.component';
+import { CreateObsComponent } from '../create-obs/create-obs.component';
+
 
 const routes:Routes = [
   {
     path: 'example',
     component: ExampleComponent
+  },
+  {
+    path: 'obser',
+    component: CreateObsComponent
   }
 ]
 
