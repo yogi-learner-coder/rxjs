3cb7e90361dbcbb503cec36395523c4d47e99b53
tiwariji <tiwariyogesht@gmail.com>
2020-02-17 18:54:13 +0530

    create app-routing module manually

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import { ExampleComponent } from '../example/example.component';
+
+const routes:Routes = [
+  {
+    path: 'example',
+    component: ExampleComponent
+  }
+]
+
+
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
